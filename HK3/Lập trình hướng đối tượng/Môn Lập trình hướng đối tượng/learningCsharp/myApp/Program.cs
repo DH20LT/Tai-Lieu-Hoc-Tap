521f4385d365fed2b7e6992ab420441c3fb8cfbd
Keios Starqua <khoi0941@gmail.com>
2022-03-06 00:18:37 +0700

    .

---
+++ HK3/Lập trình hướng đối tượng/Môn Lập trình hướng đối tượng/learningCsharp/myApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I love NGOC");
+            // comment on here
+        }
+    }
+}
